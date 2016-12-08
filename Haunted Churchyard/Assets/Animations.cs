e1e809d62d1a59d217e0281c923162caedc5b95f
pumali <patricia.l.umali@gmail.com>
2016-12-08 11:56:50 -0500

    added Unity project folder

---
+++ Haunted Churchyard/Assets/Animations.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animations : MonoBehaviour {
+
+	public Animation anim; 
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation> ();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			Debug.Log ("registered collision");
+			anim.Play (); 
+			Debug.Log ("played anim");
+		}
+	}
+}
