ad39496c7b612205995798645e9cab7f237549da
pumali <patricia.l.umali@gmail.com>
2016-12-08 14:05:30 -0500

    upload 2
    gotta remember to make additions to scene in git folder, not original
    project folder

--- Haunted Churchyard/Assets/Animations.cs
+++ Haunted Churchyard/Assets/Animations.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class Animations : MonoBehaviour {
 
 	public Animation anim; 
+	public AudioClip jetbikesound; 
+	private AudioSource source; 
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		anim = GetComponent<Animation> ();
+		source = GetComponent<AudioSource> (); 
 	}
 
 	void OnTriggerEnter(Collider other) {
@@ -15,6 +18,7 @@ public class Animations : MonoBehaviour {
 			Debug.Log ("registered collision");
 			anim.Play (); 
 			Debug.Log ("played anim");
+			source.PlayOneShot (jetbikesound, 1F); 
 		}
 	}
 }
